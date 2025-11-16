56f1cdf8d0b83c477e92f0933f615fe10b5ff7b0
Irfad C <irfadc500@gmail.com>
2025-11-16 19:00:37 +0530

    improve style

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -31,7 +31,7 @@ const App = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center h-screen">
-        <div className="flex flex-col items-center shadow-md bg-grey w-96 border border-gray-600 m-5 p-5 rounded-xl">
+        <div className="flex flex-col items-center shadow-md bg-orange-100 w-96 border  m-5 p-5 rounded-xl">
           <h2 className="text-2xl font-bold mb-4">Todo App</h2>
           <input
             className="border  p-3 rounded-lg w-4/5 focus:outline-none focus:ring-2 focus:ring-blue-500 mb-3"
@@ -43,25 +43,25 @@ const App = () => {
             onClick={() => {
               handleList();
             }}
-            className="bg-green-400 font-bold rounded m-2 p-2"
+            className="bg-green-400 font-bold rounded m-2 p-2 hover:bg-green-600"
           >
             Add
           </button>
         </div>
-        <div className="flex flex-col m-5 p-5 bg-blue-100 items-center w-96 border shadow-md mb-5 rounded">
+        <div className="flex flex-col m-5 p-5 bg-blue-100 items-center w-96  shadow-md mb-5 rounded">
           <h2 className=" rounded text-xl font-bold m-5 p-5">Task Lists</h2>
           <ul className="w-full">
             {list.map((item) => (
               <div
                 key={item.id}
-                className="flex justify-between items-center mb-3 p-3 bg-gray-100 rounded-lg"
+                className="flex justify-between items-center mb-3 p-3 bg-white rounded-lg"
               >
                 {" "}
-                <li className="text-gray-800 font-medium  rounded-xl">
+                <li className="text-black-800 font-medium  rounded-xl">
                   {item.title}
                 </li>
                 <button
-                  className="bg-red-600 text-white px-3 py-1 rounded-lg font-semibold hover:bg-red-700'"
+                  className="bg-red-500 text-white px-3 py-1 rounded-lg font-semibold hover:bg-red-700"
                   onClick={() => {
                     handleDelete(item.id);
                   }}
