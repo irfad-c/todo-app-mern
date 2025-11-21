36299a7ea8acf7012c92cbfdf25ce61781e1c120
Irfad C <irfadc500@gmail.com>
2025-11-21 21:23:04 +0530

    Make PORT configurable

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,9 @@ try {
   mongoose.connect(process.env.MONGO_URI);
   console.log("MongoDB connected");
 } catch (error) {
-  console.log("Error connecting to DB",error)
+  console.log("Error connecting to DB", error);
 }
 
-
-
 //server
-app.listen(5000, () => console.log("Server running on port localhost 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port localhost ${PORT}`));
