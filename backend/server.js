b05af4f471822f401b3dabf8228aff8e6005e0fb
Irfad C <irfadc500@gmail.com>
2025-11-21 21:20:20 +0530

    Improve the code using try catch

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import taskListRoutes from "./routes/taskListRoutes.js"
+import taskListRoutes from "./routes/taskListRoutes.js";
 
 const app = express();
 dotenv.config();
@@ -12,12 +12,16 @@ app.use(cors());
 app.use(express.json());
 
 //routes
-app.use("/api/tasks",taskListRoutes);
+app.use("/api/tasks", taskListRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+try {
+  mongoose.connect(process.env.MONGO_URI);
+  console.log("MongoDB connected");
+} catch (error) {
+  console.log("Error connecting to DB",error)
+}
+
+
 
 //server
 app.listen(5000, () => console.log("Server running on port localhost 5000"));
