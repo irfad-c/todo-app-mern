65e368afff18e8c7248a6d49b6dcc8e6b8cbb9d5
Irfad C <irfadc500@gmail.com>
2025-11-22 12:45:38 +0530

    use controllers for clean code structure

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,62 @@
+import Task from "../models/Task.js";
+
+//Create
+export async function createTask(req, res) {
+  const { task } = req.body;
+  if (!task) {
+    return res.status(400).json({ Error: "Task is required" });
+  }
+  try {
+    const newTask = await Task.post(task);
+    return res.status(201).json(newTask);
+  } catch (error) {
+    return res.status(500).json({ Error: "Error creating the task" });
+  }
+}
+
+//Read
+export async function readTask(req, res) {
+  try {
+    const getTask = await Task.find();
+    res.status(200).json(getTask);
+  } catch (error) {
+    return res.status(500).json({ Error: error.message });
+  }
+}
+
+//Update
+export async function updateTask(req, res) {
+  const { task } = req.body;
+  const { id } = req.params;
+  if (!task) {
+    return res.status(400).json({ Error: "Task is required" });
+  }
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(
+      id,
+      { task },
+      { new: true }
+    );
+    if (!updateTask) {
+      return res.status(404).json({ Error: "Task not found" });
+    }
+    return res.status(200).json(updatedTask);
+  } catch (error) {
+    return res.status(500).json({ Error: error.message });
+  }
+}
+
+//Delete
+export async function deleteTask(req, res) {
+  const { id } = req.params;
+
+  try {
+    const deletedTask = await Task.findByIdAndDelete(id);
+    if (!deletedTask) {
+      return res.status(404).json({ Error: "Task not found" });
+    }
+    return res.status(200).json(deletedTask);
+  } catch (error) {
+    return res.status(500).json({ Error: error.message });
+  }
+}
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,58 +1,17 @@
 import express from "express";
-import Task from "../models/Task.js";
+
+import {
+  createTask,
+  readTask,
+  updateTask,
+  deleteTask,
+} from "../controllers/taskController.js";
 
 const router = express.Router();
 
-//Create
-router.post("/", async (req, res) => {
-  const { task } = req.body;
-  if (!task) {
-    return res.status(400).json({ Error: "Task is required" });
-  }
-  try {
-    const newTask = await Task.create({ task });
-    res.json(newTask);
-  } catch (error) {
-    res.status(500).json({ Error: error.message });
-  }
-});
-
-//Read
-router.get("/", async (req, res) => {
-  try {
-    const tasks = await Task.find();
-    res.json(tasks);
-  } catch (error) {
-    res.status(500).json({ Error: error.message });
-  }
-});
-
-//Update
-router.put("/:id", async (req, res) => {
-  const { task } = req.body;
-  if (!task) {
-    return res.status(400).json({ Error: "Task is required" });
-  }
-  try {
-    const tasks = await Task.findByIdAndUpdate(
-      req.params.id,
-      { task },
-      { new: true }
-    );
-    res.json(tasks);
-  } catch (error) {
-    res.status(500).json({ Error: error.message });
-  }
-});
-
-//Delete
-router.delete("/:id", async (req, res) => {
-  try {
-    const task = await Task.findByIdAndDelete(req.params.id);
-    res.json({ message: "Task deleted" });
-  } catch (error) {
-    res.status(500).json({ Error: error.message });
-  }
-});
+router.post("/", createTask);
+router.get("/", readTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 export default router;
